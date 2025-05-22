019f192aeef6ffccf0e297f3b767ba79e3087a4d
ANOVA51 <novasuandhi1234@gmail.com>
2025-05-22 22:09:48 +0800

    tanpa footer

---
+++ src/components/favhiddengem.vue
@@ -0,0 +1,109 @@
+<script setup>
+import bluelagoon from '@/assets/images/hiddengem/bluelagoon.png'
+
+import { ref, computed } from 'vue'
+
+const showAll = ref(false)
+
+const toggleShow = () => {
+  showAll.value = !showAll.value
+}
+
+
+const hiddengem = [
+  {
+    image: bluelagoon,
+    name: 'Bluelagoon waterfall',
+    deskripsi: 'Air terjun tersembunyi yang indah...',
+    rating: 4.5,
+  },
+  {
+    image: bluelagoon,
+    name: 'Bluelagoon waterfall',
+    deskripsi: 'Air terjun tersembunyi yang indah...',
+    rating: 4.5,
+  },
+  {
+    image: bluelagoon,
+    name: 'Bluelagoon waterfall',
+    deskripsi: 'Air terjun tersembunyi yang indah...',
+    rating: 4.5,
+  },
+  {
+    image: bluelagoon,
+    name: 'Bluelagoon waterfall',
+    deskripsi: 'Air terjun tersembunyi yang indah...',
+    rating: 4.5,
+  },
+  {
+    image: bluelagoon,
+    name: 'Bluelagoon waterfall',
+    deskripsi: 'Air terjun tersembunyi yang indah...',
+    rating: 4.5,
+  },
+  {
+    image: bluelagoon,
+    name: 'Bluelagoon waterfall',
+    deskripsi: 'Air terjun tersembunyi yang indah...',
+    rating: 4.5,
+  },
+  {
+    image: bluelagoon,
+    name: 'Bluelagoon waterfall',
+    deskripsi: 'Air terjun tersembunyi yang indah...',
+    rating: 4.5,
+  },
+]
+const displayedItems = computed(() => {
+  return showAll.value ? hiddengem : hiddengem.slice(0, 4)
+})
+</script>
+
+<template>
+  <section class="py-10">
+    <!--Favorite Hidden Gem-->
+    <h2 class="text-5xl md:text-3xl font-black mb-10 text-center">
+      Favorite
+      <span class="text-transparent bg-clip-text bg-gradient-to-r from-green-600 to-black">
+        Hidden Gem
+      </span>
+    </h2>
+
+    <!-- Grid Card -->
+    <div class="overflow-x-auto py-10">
+      <div class="grid grid-cols-[repeat(4,auto)] gap-4 px-30" style="min-width: max-content">
+        <div
+          v-for="(item, index) in displayedItems"
+          :key="index"
+          class="bg-white rounded-xl w-40 sm:w-64 h-40 sm:h-72 flex-shrink-0 overflow-hidden drop-shadow-md hover:shadow-xl transition-shadow duration-300"
+        >
+          <div class="h-1/2 w-full">
+            <img :src="item.image" :alt="item.name" class="w-full h-full object-cover" />
+          </div>
+
+          <!--info-->
+          <div class="h-1/2 p-3 flex flex-col justify-between">
+            <p class="font-bold text-sm">{{ item.name }}</p>
+            <p class="text-xs text-gray-600 truncate">{{ item.deskripsi }}</p>
+
+            <div class="flex items-center gap-1 mt-2">
+              <span v-for="i in 5" :key="i" class="text-yellow-400 text-2xl leading-none">
+                {{ i <= Math.floor(item.rating) ? '★' : i - item.rating < 1 ? '⯪' : '☆' }}
+              </span>
+              <span class="text-xs text-gray-500 ml-1">({{ item.rating }})</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Tombol View More -->
+    <div v-if="hiddengem.length > 4" class="mt-10 flex justify-center mb-10">
+      <button 
+      @click="toggleShow"
+      class="bg-[#328E6E] text-white py-3 px-8 rounded-lg hover:bg-[#276d57] transition">
+        {{ showAll ? 'View Less' : 'View More' }}
+      </button>
+    </div>
+  </section>
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,247 @@
+<script setup>
+import IconLocation from '@/components/icons/IconLocation.vue'
+import IconMail from '@/components/icons/IconMail.vue'
+import IconWA from '@/components/icons/IconWA.vue'
+
+import kecak from '@/assets/images/kecak.jpg'
+import penari from '@/assets/images/penari.jpg'
+import statue from '@/assets/images/statue1.jpg'
+import waterfall from '@/assets/images/waterfall1.jpg'
+import { ref } from 'vue'
+
+const images = [kecak, statue, penari, waterfall]
+const currentIndex = ref(0)
+
+const nextImage = () => {
+  currentIndex.value = (currentIndex.value + 1) % images.length
+}
+
+import dimas from '@/assets/images/profile/dimas.jpg'
+import emo from '@/assets/images/profile/emo.jpg'
+import nova from '@/assets/images/profile/nova.jpg'
+import pius from '@/assets/images/profile/pius.jpg'
+
+const teamMembers = [
+  {
+    name: 'Komang Emo',
+    role: 'Frontend of TESA',
+    image: emo,
+    instagram: '#',
+    github: '#',
+    linkedin: '#',
+  },
+  {
+    name: 'Pius',
+    role: 'Backend of TESA',
+    image: pius,
+    instagram: '#',
+    github: '#',
+    linkedin: '#',
+  },
+  {
+    name: 'Novanka',
+    role: 'Backend of TESA',
+    image: nova,
+    instagram: '#',
+    github: '#',
+    linkedin: '#',
+  },
+  {
+    name: 'Gung Dimas',
+    role: 'Backend of TESA',
+    image: dimas,
+    instagram: '#',
+    github: '#',
+    linkedin: '#',
+  },
+  {
+    name: 'Novanka',
+    role: 'Backend of TESA',
+    image: dimas,
+    instagram: '#',
+    github: '#',
+    linkedin: '#',
+  },
+]
+</script>
+
+<template>
+  <div>
+    <Navbar />
+
+    <!-- Hero Section -->
+    <section class="relative h-screen w-full">
+      <img
+        src="@/assets/images/waterfall1.jpg"
+        alt="Hero Banner"
+        class="absolute inset-0 w-full h-full object-cover object-bottom brightness-75 z-0"
+      />
+    </section>
+
+    <!-- Mission Section -->
+    <section class="relative px-6 md:px-30 py-10 overflow-hidden">
+      <!-- Gambar latar Pulau Bali -->
+      <img
+        src="@/assets/images/pulaubali.png"
+        alt="Bali Background"
+        class="absolute inset-0 w-full h-full object-contain opacity-20 z-0 pointer-events-none py-5"
+      />
+
+      <div class="flex flex-col md:flex-row gap-10 md:gap-75 items-center">
+        <!-- Text -->
+        <div class="md:w-3/4 text-lg text-gray-700">
+          <h2 class="text-4xl md:text-5xl font-black mb-10 whitespace-nowrap">
+            Why
+            <span class="text-transparent bg-clip-text bg-gradient-to-r from-green-600 to-black">
+              Choose Us
+            </span>
+          </h2>
+          <p>
+            We collaborate closely with local communities to promote a more inclusive and
+            sustainable tourism experience. By working hand-in-hand with residents, we ensure that
+            tourism development respects local traditions, preserves cultural heritage, and
+            contributes directly to the well-being of the people. Our platform is dedicated to
+            showcasing the hidden gems of the region—untouched natural wonders, authentic cultural
+            experiences, traditional arts, and local cuisines that are often overlooked by
+            mainstream tourism.
+          </p>
+        </div>
+
+        <!-- Carousel Bertumpuk -->
+        <div
+          class="relative w-full max-w-[360px] max-w-[360px] mx-auto h-[250px] cursor-pointer"
+          @click="nextImage"
+        >
+          <div class="relative w-full h-full">
+            <div
+              v-for="(img, index) in images"
+              :key="img"
+              class="absolute top-0 transition-all duration-500 ease-in-out rounded-xl overflow-hidden shadow-lg"
+              :class="[
+                index === currentIndex
+                  ? 'z-30 scale-100 left-0'
+                  : index === (currentIndex + 1) % images.length
+                    ? 'z-20 scale-95 left-[25%]' // Gambar kedua muncul sebagian
+                    : 'z-10 scale-90 left-[25%]',
+              ]"
+              style="width: 100%; height: 100%"
+            >
+              <img
+                :src="img"
+                class="w-full h-full object-cover rounded-xl"
+                :alt="`Image ${index + 1}`"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!--The Creator Of TESA-->
+    <section class="px-6 md:px-30 py-10">
+      <h2 class="text-3xl md:text-4xl font-bold text-center mb-10">
+        The Creator
+        <span class="text-transparent bg-clip-text bg-gradient-to-r from-green-600 to-black">
+          TESA
+        </span>
+      </h2>
+
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4 place-items-center">
+        <div
+          v-for="(member, index) in teamMembers"
+          :key="index"
+          class="bg-gray-200 p-4 rounded-xl text-center shadow-md w-60 h-70"
+        >
+          <div class="absolute-top-12 flex justify-center">
+            <img
+              :src="member.image"
+              :alt="member.name"
+              class="w-24 h-24 rounded-full border-4 border-white object-cover shadow-md"
+            />
+          </div>
+
+          <h3 class="text-lg font-bold mt-3">{{ member.name }}</h3>
+          <p class="text-sm text-gray-600">{{ member.role }}</p>
+          <div class="flex justify-center mt-3 space-x-3 text-gray-700 text-xl">
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-github"></i>
+            <i class="fab fa-linkedin"></i>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section class="px-6 md:px-30 mt-24 sm:mt-30">
+      <div class="flex flex-col md:flex-row gap-10 md:gap-8">
+        <!-- Info -->
+        <div class="flex flex-col">
+          <h1 class="text-4xl sm:text-[42px] font-semibold leading-12 sm:leading-[62px] font-se">
+            <span class="text-transparent bg-clip-text bg-gradient-to-r from-green-600 to-black">
+              Information Contact
+            </span>
+          </h1>
+          <p class="text-neu-600 text-sm sm:text-base mb-6">
+            We are available to answer your inquiries or suggestions. Get in touch with us, and our
+            team will respond as soon as possible.
+          </p>
+          <ul class="flex flex-col gap-6">
+            <li class="flex items-center gap-2 text-sm sm:text-base font-medium">
+              <IconWA class="size-5 sm:size-5" />+62 812-3456-7890
+            </li>
+            <li class="flex items-center gap-2 text-sm sm:text-base font-medium">
+              <IconMail class="size-5 sm:size-6" />Swastyastu@TESA.id
+            </li>
+            <li class="flex items-center gap-2 text-sm sm:text-base font-medium">
+              <IconLocation class="size-5 sm:size-6" />Jl. Raya Nara No.123, Denpasar, Bali 80232,
+              Indonesia
+            </li>
+          </ul>
+        </div>
+
+        <!-- Contact Form -->
+        <div
+          class="p-4 sm:p-6 rounded-3xl min-w-full md:min-w-100 xl:min-w-[480px] border border-neu-200"
+        >
+          <h4 class="font-semibold text-2xl sm:text-[32px]">Contact Us</h4>
+          <p class="text-neu-600 text-sm sm:text-base mb-6">
+            Feel free to contact us at your convenience.
+          </p>
+          <form class="flex flex-col gap-4">
+            <div class="flex gap-4">
+              <input
+                type="text"
+                placeholder="Firstname"
+                class="px-3 w-1/2 py-3 text-sm font- border placeholder:text-neu-500 border-neu-200 rounded-full"
+              />
+              <input
+                type="text"
+                placeholder="Lastname"
+                class="px-3 w-1/2 py-3 text-sm border placeholder:text-neu-500 border-neu-200 rounded-full"
+              />
+            </div>
+            <input
+              type="email"
+              placeholder="Email"
+              class="px-3 py-3 text-sm border placeholder:text-neu-500 border-neu-200 rounded-full"
+            />
+            <input
+              type="text"
+              placeholder="Phone"
+              class="px-3 py-3 text-sm border placeholder:text-neu-500 border-neu-200 rounded-full"
+            />
+            <textarea
+              rows="7"
+              placeholder="Message"
+              class="px-3 py-3 text-sm placeholder:text-neu-500 border border-neu-200 rounded-3xl"
+            ></textarea>
+            <button type="submit" class="bg-pr-500 text-neu-50 px-6 py-3 font-medium rounded-full">
+              Send
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+    <div></div>
+  </div>
+</template>
